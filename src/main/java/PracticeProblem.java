d59a0612b170a9f230bc83e2aaab83071e6f09cc
kyler-huang-ics4u1-01-25 <kyler.huang@student.tdsb.on.ca>
2025-09-30 19:07:34 +0000

    update practiceProblems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,19 +1,63 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		int[][] intArray2D1 = {
+			{1,2,3,4,5},
+			{2,4,6,8,10},
+			{11,22,33,44,55}
+		};
 
+		int[][] intArray2D2 = {
+			{101239,2,100,4,5},
+			{8,10},
+			{440203,552222, 129192}
+		};
+
+		int[][] intArray2D3 = {
+			{9,2,0,4,5},
+			{8,10, 4, 5, 1},
+			{1,5, 2, 3, 4}
+		};
+
+		sum2D(intArray2D1);
+
+		int sumRowResult = sumRow(intArray2D2, 0);
+		System.out.println(sumRowResult);
+
+		int sumColumnResult = sumColumn(intArray2D3, 1);
+		System.out.println(sumColumnResult);
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static void sum2D(int[][] _array2D) {
+		int sum = 0;
+		for (int i = 0; i < _array2D.length; i++){
+			for (int j = 0; j < _array2D[i].length; j++){
+				int currentValue = _array2D[i][j];
+				sum += currentValue;
+			}
+		}
+		System.out.println(sum);
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public static int sumRow(int[][] _array2D2, int rowIndex) {
+		int sum = 0;
+		for (int i = 0; i < _array2D2[rowIndex].length; i++){
+			int currentValue = _array2D2[rowIndex][i];
+			sum += currentValue;
+		}
+		return sum;
 	}
 
-	public static void q3() {
-		//Write question 3 code here
+	public static int sumColumn(int[][] _array2D3, int columnIndex) {
+		int sum = 0;
+		for (int i = 0; i < _array2D3.length; ++i){
+			if (columnIndex < _array2D3[i].length) {
+				int currentValue = _array2D3[i][columnIndex];
+				sum+=currentValue;
+			}
+		}
+
+		return sum;
 	}
 
 	public static void q4() {
